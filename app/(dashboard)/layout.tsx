473f2509d877c18ebdf19c65708ea6553cf3b655
h_ri <h_ri@satt.jp>
2024-04-04 16:02:46 +0900

    layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
-import Navbar from "./components/navbar";
-import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar/navbar";
+import Sidebar from "./components/sidebar/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="md:pl-56">
-        <Navbar />
-      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
+      </div>
+      <div className="md:pl-56">
+        <Navbar />
       </div>
       <main className="md:pl-56"> {children}</main>
     </div>
