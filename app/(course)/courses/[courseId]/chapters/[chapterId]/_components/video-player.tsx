64fcd5e1fcee95faf629e165ef5c27cb231d51b5
h_ri <h_ri@satt.jp>
2024-07-18 18:47:35 +0900

    video auto complete

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
 import { cn } from "@/lib/utils";
 import MuxPlayer from "@mux/mux-player-react";
+import axios from "axios";
 import { Loader2, Lock } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 type Props = {
   chapterId: string;
@@ -24,6 +28,31 @@ const VideoPlayer = ({
   completeOnEnd,
 }: Props) => {
   const [isReady, setIsReady] = useState(false);
+  const confetti = useConfettiStore();
+  const router = useRouter();
+  const onEnd = async () => {
+    try {
+      if (completeOnEnd) {
+        await axios.put(
+          `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+          {
+            isCompleted: true,
+          }
+        );
+        if (!nextChapterId) {
+          confetti.onOen();
+        }
+        toast.success("chapter progress updated");
+        router.refresh();
+        if (nextChapterId) {
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+        }
+      }
+    } catch (error) {
+      toast.error("someting went wrong");
+    }
+    // 只对没有completed
+  };
   return (
     <div className="relative aspect-video">
       {!isReady && !isLocked && (
@@ -44,6 +73,7 @@ const VideoPlayer = ({
           onCanPlay={() => setIsReady(true)}
           autoPlay
           playbackId={playbackId}
+          onEnded={onEnd}
         />
       )}
     </div>
